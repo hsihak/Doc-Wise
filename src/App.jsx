d23e371e6eb17d2bebbb067a457b40c44c113ef7
Hangsihak Sin <Hsin9104@conestogac.on.ca>
2023-11-23 19:36:29 -0500

    add: create sub header menu for phase one

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './assets/pages/Home'
+import PhaseOne from './assets/pages/PhaseOne'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
+          <Route path='/phase-one' element={<PhaseOne/>}></Route>
         </Routes>
       </Router>
     </>
---
+++ src/assets/pages/PhaseOne.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from '../components/Header/Header'
+import SubHeader from '../components/Header/SubHeader'
+
+const PhaseOne = () => {
+  return (
+    <div>
+        <Header/>
+        {/* Sub header */}
+        <SubHeader/>
+    </div>
+  )
+}
+
+export default PhaseOne
