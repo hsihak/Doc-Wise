56d725f4ae84ecf31cc3eb984b486c4328aaba17
Hangsihak Sin <Hsin9104@conestogac.on.ca>
2023-11-23 21:40:02 -0500

    add: create drag and drop feature

---
+++ src/assets/components/DropMenu/FileUpload.jsx
@@ -0,0 +1,259 @@
+import React, { useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { Box, Button, IconButton, Typography } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear'; // Import the 'Clear' icon
+import Modal from '@mui/material/Modal';
+
+export default function FileUpload(props) {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
+  const [fileToDelete, setFileToDelete] = useState(null); // Track the file to delete
+  const [deleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
+  const { getRootProps, getInputProps, open, isDragActive } = useDropzone({
+    // Disable click and keydown behavior
+    noClick: true,
+    noKeyboard: true,
+    onDrop: (files) => handleFileUpload(files),
+  });
+
+  const handleFileUpload = (files) => {
+    // Do something with the files (e.g., upload to server)
+    setUploadedFiles([...uploadedFiles, ...files]);
+  };
+
+  const openDeleteConfirmation = (file) => {
+    setFileToDelete(file);
+    setDeleteConfirmationOpen(true);
+  };
+
+  const closeDeleteConfirmation = () => {
+    setFileToDelete(null);
+    setDeleteConfirmationOpen(false);
+  };
+
+  const confirmDelete = () => {
+    if (fileToDelete) {
+      removeFile(fileToDelete);
+      closeDeleteConfirmation();
+    }
+  };
+
+  const removeFile = (file) => {
+    const updatedFiles = uploadedFiles.filter((uploadedFile) => uploadedFile !== file);
+    setUploadedFiles(updatedFiles);
+  };
+
+  const canCompareSimilarities = uploadedFiles.length >= 2;
+  const isAtleastOneFileUploaded = uploadedFiles.length == 1;
+
+  const filesList = uploadedFiles.map((file) => (
+    <Box
+      sx={{
+        marginBlock: '10px',
+      }}
+      key={file.path}
+    >
+      <li>
+        <Box
+          sx={{
+            background: '#F5F5F5',
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingInline: '2px',
+            borderRadius: '10px',
+            border: '1px solid #707070',
+          }}
+        >
+          {file.path}
+          <IconButton onClick={() => openDeleteConfirmation(file)}>
+            <ClearIcon />
+          </IconButton>
+        </Box>
+      </li>
+    </Box>
+  ));
+
+  
+
+  return (
+    <Box
+      className="container"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: '20px',
+      }}
+    >
+      <div {...getRootProps({ className: 'dropzone' })} style={dropzoneStyles}>
+        <input {...getInputProps()} />
+        {isDragActive ? (
+            <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}>
+                <p>Drop the files here ...</p>
+            </Box>
+        ) : (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <img
+              src="src\assets\Drag-And-Drop.svg"
+              alt="Drag-And-Drop-Image"
+              width="100px"
+            />
+            <p>Or</p>
+            <Button
+              variant="contained"
+              sx={{
+                background: '#D3D9CE',
+                color: 'black',
+                border: '2px solid black',
+                '&:hover': {
+                  background: '#D3D9CE',
+                },
+              }}
+              onClick={open}
+            >
+              Browse File
+            </Button>
+            <p>Formats supported: docx and PDF</p>
+          </Box>
+        )}
+      </div>
+      <Box
+        sx={{
+          marginTop: '20px',
+          width: '30%',
+          display: 'flex',
+          gap: '10px',
+          flexDirection: 'column'
+        }}
+      >
+        <h4>Files uploaded:</h4>
+        <ul>{filesList}</ul>
+      </Box>
+
+    {canCompareSimilarities && (
+        <Button
+      variant="contained"
+      sx={{
+        background: '#416E71',
+            color: 'white',
+            border: '2px solid black',
+            '&:hover': {
+          background: '#D3D9CE',
+        },}}
+      >Compare Similarities</Button>
+    ) 
+    }
+
+    { isAtleastOneFileUploaded && (
+            <Box
+                sx={{
+                    padding: '30px',
+                    textAlign: 'center'
+                }}>
+                <Typography 
+                    sx={{
+                        color: 'red'
+                    }}>Please select at least two documents in order to compare their similarities.</Typography>
+                <Typography 
+                    sx={{
+                        color: 'red',
+                    }}>You can select as many files as you want.</Typography>
+            </Box>
+
+    )}
+
+    {/* Delete Confirmation Modal */}
+          <Modal
+        open={deleteConfirmationOpen}
+        onClose={closeDeleteConfirmation}
+        aria-labelledby="delete-confirmation-title"
+        aria-describedby="delete-confirmation-description"
+      >
+        <Box sx={modalStyles}>
+        <Box
+        sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '30px'
+        }}>
+        <Typography
+        sx={{
+            textAlign: 'center'
+        }}>
+            Are you sure you want to delete <span className=' underline block text-center'>
+                {fileToDelete ? fileToDelete.path : ''} ?
+                </span>
+        </Typography>
+        <Box
+        sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+        }}>
+          <Button 
+                variant="contained"
+                sx={{
+                  background: '#ffffff',
+                      color: 'black',
+                      border: '1px solid black',
+                      '&:hover': {
+                    background: '#D3D9CE',
+                  },}}
+          onClick={closeDeleteConfirmation}>Cancel</Button>
+          <Button 
+                          variant="contained"
+                          sx={{
+                            background: '#416E71',
+                                color: 'white',
+                                border: '1px solid black',
+                                '&:hover': {
+                              background: '#D3D9CE',
+                            },}}
+          onClick={confirmDelete}>Delete</Button>
+        </Box>
+        </Box>
+
+        </Box>
+      </Modal>
+    </Box>
+  );
+}
+
+const dropzoneStyles = {
+  width: '30%',
+  height: '250px',
+  padding: '20px',
+  borderWidth: '4px',
+  borderRadius: '4px',
+  borderStyle: 'dashed',
+  borderColor: 'black', // Set the dashed line color to black
+  backgroundColor: '#fafafa',
+  color: 'black',
+  outline: 'none',
+  transition: 'border .24s ease-in-out',
+};
+
+const modalStyles = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+  };
--- src/assets/pages/PhaseOne.jsx
+++ src/assets/pages/PhaseOne.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
 import { Header } from '../components/Header/Header'
 import SubHeader from '../components/Header/SubHeader'
+import FileUpload from '../components/DropMenu/FileUpload'
+import { Box, Typography } from '@mui/material'
 
 const PhaseOne = () => {
+
+    const handleFileUpload = files => {
+        // Handle file upload logic here
+        console.log('Uploaded files:', files);
+      };
+
   return (
     <div>
         <Header/>
         {/* Sub header */}
         <SubHeader/>
+        {/* File Dropzone */}
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                paddingBlock: '20px'
+            }}>
+            <Typography>
+                Drag and Drop Files Here
+            </Typography>
+        </Box>
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+            }}>
+            <FileUpload onFileUpload={handleFileUpload}/>
+        </Box>
     </div>
   )
 }
